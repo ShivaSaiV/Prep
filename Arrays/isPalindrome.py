dff14bba41f76669e0322d8044f349ce027c5c3b
Shiva Sai Vummaji <106600142+ShivaSaiV@users.noreply.github.com>
2023-08-10 23:35:02 -0400

    Update isPalindrome.py

--- Arrays/isPalindrome.py
+++ Arrays/isPalindrome.py
@@ -1,7 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         st = []
+        # Remove special characters and spaces
         for char in s:
             if char.isalnum():
                 st.append(char.lower())
+                
         return st == st[::-1]

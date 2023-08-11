615b4f18728b93e8e1745f4b14e521913b6de4e7
Shiva Sai Vummaji <106600142+ShivaSaiV@users.noreply.github.com>
2023-08-10 23:34:35 -0400

    Delete isPalindrome.py

--- isPalindrome.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        st = []
-        for char in s:
-            if char.isalnum():
-                st.append(char.lower())
-        return st == st[::-1]

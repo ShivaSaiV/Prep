22aa354c82aa444b76a31fa9d1c700e420b4ac8c
Shiva Sai Vummaji <106600142+ShivaSaiV@users.noreply.github.com>
2023-08-10 23:54:01 -0400

    Create sqrt_x.py

---
+++ Arrays/sqrt_x.py
@@ -0,0 +1,14 @@
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+# You must not use any built-in exponent function or operator.
+
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(1, x+1):
+            if i * i == x:
+                return i
+            elif i * i > x:
+                return i - 1
+        return 0

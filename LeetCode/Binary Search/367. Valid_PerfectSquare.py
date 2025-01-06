87bd47ae987d382a014da3f5ab866cd350c8d55f
ShivaSaiV <svummaji@purdue.edu>
2025-01-06 11:35:53 -0500

    367

--- LeetCode/Binary Search/367. Valid_PerfectSquare.py
+++ LeetCode/Binary Search/367. Valid_PerfectSquare.py
@@ -9,4 +9,19 @@ You must not use any built-in library function, such as sqrt.
 
 class Solution(object):
     def isPerfectSquare(self, num):
+        l = 1
+        r = num
+
+        while l <= r:
+            mid = l + ((r - l) // 2)
+
+            if mid * mid == num:
+                return True
+            
+            if mid * mid > num:
+                r = mid - 1
+            
+            else:
+                l = mid + 1
         
+        return False

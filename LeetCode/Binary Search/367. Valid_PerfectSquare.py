50c5c92cbbd2618ec07d26023fe4c7f44c95f9f1
ShivaSaiV <svummaji@purdue.edu>
2025-01-06 10:46:30 -0500

    367

---
+++ LeetCode/Binary Search/367. Valid_PerfectSquare.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/valid-perfect-square/description/
+# Difficulty: easy
+
+'''
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+You must not use any built-in library function, such as sqrt.
+'''
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        
